ba082e10d3ad60c4c8c28f5f1e7b84e8fa880120
Bruno Oliveira <nicoddemus@gmail.com>
2014-08-25 20:21:51 -0300

    adding test for main script
    fixes #9

--- bender/_main.py
+++ bender/_main.py
@@ -4,11 +4,15 @@ from ._bender import Bender
 from bender.backbones.console import BenderConsole
 
 
+def get_console():
+    return BenderConsole()
+
+
 def main(argv=None):
     if argv is None:
         # later we will probably use this to configure which backbone to use
         argv = sys.argv
-    bot = Bender(BenderConsole())
+    bot = Bender(get_console())
     bot.loop()
     return 0
 
--- bender/backbones/console.py
+++ bender/backbones/console.py
@@ -11,34 +11,38 @@ from bender.decorators import backbone_start, backbone_shutdown
 class BenderConsole(object):
 
 
-    def __init__(self):
+    def __init__(self, stdout=sys.stdout, stdin=sys.stdin):
+        self._stdout = stdout
+        self._stdin = stdin
         self.on_message_received = None
         self._thread = None
 
     @backbone_start
     def start(self):
-        self._send_message('Hey, my name is Bender. Can I help ya?')
+        self._send_message(u'Hey, my name is Bender. Can I help ya?')
         self._thread = threading.Thread(target=self._raw_input)
         self._thread.start()
 
     @backbone_shutdown
     def shutdown(self):
-        print('Hit keyboard interrupt to get out')
+        self._stdout.write(u'Hit keyboard interrupt to get out\n')
         try:
             self._thread.join()
         except KeyboardInterrupt:
             pass
 
     def _send_message(self, text):
-        print('\n' + text)
+        self._stdout.write(u'\n' + text + u'\n')
 
     def _raw_input(self):
         while True:
             try:
-                user_input = raw_input('\n> ')
-            except EOFError:
+                self._stdout.write(u'\n> ')
+                user_input = self._stdin.readline()
+            except (EOFError, ValueError):
                 return
             if user_input:
+                self._stdout.flush()
                 msg = ConsoleMessage(self, user_input)
                 self.on_message_received(msg)
 
