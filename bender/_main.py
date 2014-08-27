f7d25b0992a9719862b7e6aa83d77054bfda88cd
Bruno Oliveira <nicoddemus@gmail.com>
2014-08-26 21:49:16 -0300

    selecting backbone from command line

--- bender/_main.py
+++ bender/_main.py
@@ -1,4 +1,6 @@
+import argparse
 import sys
+import pkg_resources
 
 from bender.backbones.console import BenderConsole
 
@@ -13,11 +15,31 @@ def get_brain():
     return None
 
 
+def get_setuptools_backbones():
+    return {p.name: p.load() for p in
+            pkg_resources.iter_entry_points('bender_backbone')}
+
+
 def main(argv=None):
     if argv is None:
         # later we will probably use this to configure which backbone to use
         argv = sys.argv
-    bot = Bender(get_console())
+
+    available_backbones = get_setuptools_backbones()
+    console_name = 'console'
+
+    parser = argparse.ArgumentParser(description='Bender chat bot')
+    parser.add_argument('--backbone', default=console_name,
+                        choices=[console_name] + sorted(available_backbones),
+                        help='name of the backbone to use')
+    args = parser.parse_args(argv[1:])
+
+    if args.backbone == console_name:
+        backbone = get_console()
+    else:
+        backbone_class = available_backbones[args.backbone]
+        backbone = backbone_class()  # instantiate class
+    bot = Bender(backbone)
     bot.loop()
     return 0
 
--- bender/_tests/test_main.py
+++ bender/_tests/test_main.py
@@ -1,10 +1,12 @@
 from io import StringIO
+import pkg_resources
 import pytest
 import threading
 
 import bender._main
 from bender.backbones.console import BenderConsole
-from bender.testing import VolatileBrain
+from bender.decorators import backbone_start
+from bender.testing import VolatileBrain, DumbMessage
 
 
 @pytest.mark.timeout(3.0)
@@ -19,5 +21,42 @@ def test_main(mock):
     console = BenderConsole(stdout=stdout, stdin=stdin)
     mock.patch.object(bender._main, 'get_console', return_value=console)
     mock.patch.object(bender._main, 'get_brain', return_value=VolatileBrain())
-    assert bender._main.main() == 0
+    assert bender._main.main([]) == 0
     assert 'Hey, my name is Bender' in stdout.getvalue()
+
+
+@pytest.mark.timeout(3.0)
+def test_backbone_selection(mock):
+    class EntryPoint(object):
+        pass
+
+    backbone = QuitterBackbone()
+    get = lambda: backbone
+
+    p = EntryPoint()
+    p.name = 'quitter'
+    p.load = lambda: get
+
+    original_entry_points = pkg_resources.iter_entry_points
+    def iter_entry_points(name):
+        if name == 'bender_backbone':
+            return [p]
+        else:
+            return original_entry_points(name)
+
+    mock.patch.object(pkg_resources, 'iter_entry_points', iter_entry_points)
+    mock.patch.object(bender._main, 'get_brain', return_value=VolatileBrain())
+    assert bender._main.main(['', '--backbone', 'quitter']) == 0
+    assert backbone.started
+
+
+class QuitterBackbone(object):
+
+    def __init__(self):
+        self.on_message_received = None
+        self.started = False
+
+    @backbone_start
+    def start(self):
+        self.on_message_received(DumbMessage('quit', 'user'))
+        self.started = True
